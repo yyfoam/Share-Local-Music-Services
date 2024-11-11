6999c6aca9d0d6fd0b86f8f51075ede49b672305
YYFOAM <111266669+yyfoam@users.noreply.github.com>
2024-11-11 19:53:39 +0800

    Update generate_config.py

--- generate_config.py
+++ generate_config.py
@@ -6,12 +6,12 @@ def generate_config_file(config_path):
     if not os.path.exists(config_path):
         # 配置文件内容
         config_content = """
-"
-目前测试可以播放的格式
-'.flac', '.wav', '.mp3', '.ape', '.m4a', '.dsf', '.ogg', '.aiff', '.opus', '.aac', 
-'.aif','.aifc', '.wma', '.adx', '.au' ,'.caf', '.mp2', '.rm', '.oga', '.tta', 
-'.voc', '.mka'
-"
+
+# 目前测试可以播放的格式
+# '.flac', '.wav', '.mp3', '.ape', '.m4a', '.dsf', '.ogg', '.aiff', '.opus', '.aac', 
+# '.aif','.aifc', '.wma', '.adx', '.au' ,'.caf', '.mp2', '.rm', '.oga', '.tta', 
+#'.voc', '.mka'
+
 
 # 时区设置
 china_tz = 'Asia/Shanghai'
@@ -19,13 +19,14 @@ china_tz = 'Asia/Shanghai'
 # 设置缓存文件的目录
 CACHE_DIR = "cache_dir"
 
+# ff无法拿到元数据的格式
 ff_exclude_metadata = ['.wav', ]
 
+# ff无法拿到图像的格式
 ff_exclude_img = ['.wma', '.wav']
 
 # 需要转码的格式
 decoding_format = ['.ape', '.m4a', '.dsf', '.aiff', '.aif','.aifc', '.wma', '.adx', '.au' ,'.caf', '.mp2', '.rm', '.oga', '.tta', '.voc']
-
 
 # 日志级别: DEBUG > INFO > WARNING > ERROR
 log_level = 'INFO'
